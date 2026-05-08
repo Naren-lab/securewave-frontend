33b447e41b2b0dfbfd45c4520442afd74e3e0ad9
Narendra Babu <narendrab077@gmail.com>
2026-05-08 14:49:35 +0530

    fix group chat page build error

---
+++ app/group-chat/page.tsx
@@ -0,0 +1,7 @@
+export default function GroupChatPage() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      Group Chat Coming Soon
+    </div>
+  );
+}
